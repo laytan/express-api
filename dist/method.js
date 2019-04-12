5d86c5a28fa4aa6071af2003bfdd0beca38a58fa
laytan <Laytanlaats@hotmail.com>
2019-04-12 15:01:02 +0200

    Removed /dist

--- dist/method.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Method;
-(function (Method) {
-    Method[Method["get"] = 0] = "get";
-    Method[Method["post"] = 1] = "post";
-})(Method = exports.Method || (exports.Method = {}));
-//# sourceMappingURL=method.js.map
